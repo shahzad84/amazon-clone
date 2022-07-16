a6182bce2d9eafd36444710646781d87d839c6b2
Shahzad <shahzad20022002@gmail.com>
2022-07-16 12:39:16 +0530

    First commit

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+function Header() {
+    
+  return (
+    <div className="header">
+     <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav"></div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAcW2GQ1WZJh4CBPJNqHxVpsZuuC256F3s",
+    authDomain: "fir-ff911.firebaseapp.com",
+    projectId: "fir-ff911",
+    storageBucket: "fir-ff911.appspot.com",
+    messagingSenderId: "732100406083",
+    appId: "1:732100406083:web:6ee28774fc6c3443b91df6",
+    measurementId: "G-1167KGCPZG"
+  };
